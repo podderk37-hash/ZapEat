5be8afb41005d0bf8ca8353a5f54eda1de86aa19
podderk37-hash <podderk37@gmail.com>
2025-11-14 19:06:01 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -33,8 +33,8 @@ export const signUp = async (req, res) => {
 
         const token = await genToken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -63,8 +63,8 @@ export const signIn = async (req, res) => {
 
         const token = await genToken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -171,8 +171,8 @@ export const googleAuth = async (req,res) => {
 
         const token = await genToken(user._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
