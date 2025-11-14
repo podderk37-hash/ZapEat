a37f05a857075ef049a23d68ca196a39f130b1db
podderk37-hash <podderk37@gmail.com>
2025-11-14 20:07:08 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -79,7 +79,11 @@ export const signIn = async (req, res) => {
 
 export const signOut = async (req, res) => {
     try {
-        res.clearCookie("token");
+        res.clearCookie("token",{
+            httpOnly:true,
+            secure:true,
+            sameSite:"none"
+        });
         return res.status(200).json({ message: " Logout Successfully" })
     } catch (error) {
         return res.status(500).json(`sign out error ${error}`);
